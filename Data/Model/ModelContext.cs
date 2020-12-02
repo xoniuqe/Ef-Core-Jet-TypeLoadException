0b6f00e65d92cd4961fb47c3f06be4f42ca8cbf5
Tobias Arens <arens@jam-software.de>
2020-12-02 12:33:45 +0100

    repro for TypeLoaderException

---
+++ Data/Model/ModelContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreJetTesst.Model
+{
+    public class ModelContext : DbContext
+    {
+        public virtual DbSet<Model> Models { get; set; }
+
+        public ModelContext()
+        {
+        }
+        public ModelContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = "\\test.mdb";
+            var connectionString =
+                "Provider=Microsoft.Jet.OLEDB.4.0;Jet OLEDB:Engine Type=5;User ID=Admin; Data Source=\"{0}\";OLE DB Services = 0;Mode=Share Deny None;Jet OLEDB:Database Password=\"PASSWORD\";";
+            optionsBuilder.UseJetOleDb(string.Format(connectionString, dbPath));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model>();
+        }
+    }
+
+    public class Model
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Model() { }
+
+
+        public Model(int pID, string pName)
+        {
+            ID = pID;
+            Name = pName;
+        }
+    }
+}
---
+++ EFCoreJetTesst/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using EFCoreJetTesst.Model;
+
+namespace EFCoreJetTesst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lAsb = Assembly.Load("System.Data.OleDb");
+            var lType = Type.GetType("System.Data.OleDb.OleDbFactory, System.Data.OleDb");
+            Console.WriteLine("Hello World!");
+            using (var context = new ModelContext())
+            {
+                context.Database.EnsureCreated();
+                var lModels = context.Models.ToList();
+                context.Models.Add(new Model.Model(new Random().Next(), "NAME BLABLA"));
+                context.SaveChanges();
+            }
+        }
+    }
+}
